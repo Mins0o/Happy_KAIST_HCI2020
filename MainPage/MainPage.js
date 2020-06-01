6111e8fc178037764efcaddf9d8b35d5502eb362
MINSOO KANG <stengine2@gmail.com>
2020-06-01 19:01:25 +0900

    Profile header bar

--- MainPage/MainPage.js
+++ MainPage/MainPage.js
@@ -1,3 +1,21 @@
+var userName="Minsoo Kang";
+var headerBar = document.getElementById("headerBar");
+var sticky = headerBar.offsetTop;
+window.onscroll = function() {stickyScrolling()};
+
+
 $( document ).ready(function() {
+	document.getElementById("userName").innerHTML="Hi " + userName;
+});
+
+function displayItems(){
 	
+}
+
+function stickyScrolling() {
+  if (window.pageYOffset >= sticky) {
+    headerBar.classList.add("sticky")
+  } else {
+    headerBar.classList.remove("sticky");
+  }
 }
