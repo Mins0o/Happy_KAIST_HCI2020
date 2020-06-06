effb258dfcd1cd9ad95338a6bd8068a0bd612a5c
bellagodiva <bellagodiva080105@gmail.com>
2020-06-06 10:30:00 +0900

    Create Recipe_image.js

---
+++ Recipe/Recipe_image.js
@@ -0,0 +1,10 @@
+var image_pairs = [
+    { 
+        "name": "burger",
+        "source": "./image/burger.jpg"
+    },
+    { 
+        "name": "salmonteriyaki",
+        "source": "./image/salmonteriyaki.jpg"
+    }
+]
