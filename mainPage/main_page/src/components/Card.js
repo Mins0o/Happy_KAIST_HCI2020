f58487d8cf5e40e37b47d58e66bd19aa60f7a764
MINSOO KANG <stengine2@gmail.com>
2020-06-06 17:09:37 +0900

    Still trying stuffs with data

--- mainPage/main_page/src/components/Card.js
+++ mainPage/main_page/src/components/Card.js
@@ -10,11 +10,11 @@ class CardItem extends React.Component {
 	render() {
 		return (
 			<Card
-				style={{width : "18rem"}}
+				style={{width : "12rem"}}
 			>
-				<Card.Img variant="top" src={require("../image_sources/0"+(this.props.numbering).toString()+".png")} />
+				<Card.Img variant="top" src={require("../image_sources/"+(this.props.numbering).toString()+".png")} />
 				<Card.Body>
-					<Card.Text>Some quick example text to build on the card title and make up the bulk of the card's content.
+					<Card.Text>{require('../data/food_data.json')[this.props.numbering-1].name}
 					</Card.Text>
 				</Card.Body>
 			</Card>
