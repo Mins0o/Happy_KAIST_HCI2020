6cd86b176f2344b49535ed7aeb15022897926aee
bellagodiva <bellagodiva080105@gmail.com>
2020-06-08 01:29:58 +0900

    Update App.js

--- Recipe/recipe/src/App.js
+++ Recipe/recipe/src/App.js
@@ -5,28 +5,34 @@ import './App.css';
 //import './styles.css';
 import { render } from '@testing-library/react';
 
+
 import Tabs from 'react-bootstrap/Tabs';
 import Tab from 'react-bootstrap/Tab';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import Container from 'react-bootstrap/Container';
 import Row from 'react-bootstrap/Row';
 import Col from 'react-bootstrap/Col';
+import Button from 'react-bootstrap/Button';
 
-import Images from './components/Images';
 
-//import Tabs from './components/Tabs';
+import ImagesIngredients from './components/ImagesIngredients';
+import ImagesUtensils from './components/ImagesUtensils';
+
 
 class App extends React.Component {
   constructor(props){
     super(props);
     this.state = {
       ingredients:['anjeng', 'anjeng', 'anjeng'],
-      //utensils: [],
+      utensils: ['pan', 'pan', 'pan', 'pan'],
+      inCount: 0,
       //recipe: 'this is the recipe'
     }
   }
   render(){
     const { ingredients } = this.state;
+    const { utensils } = this.state;
+    const { inCount } = this.state;
     return (
       <div className="App">
         <br></br>
@@ -37,8 +43,10 @@ class App extends React.Component {
             <img src={burger} alt="Burger"/>
             </Col>
             <Col><div id="title">Burger</div><br></br>
-            <button id="checkout">Proceed to Checkout</button></Col>
-            <Col> </Col>
+            <Button variant="primary">Proceed to Checkout</Button>{' '}</Col>
+            <Col>
+            
+            </Col>
           </Row>
           <Row><Col></Col></Row>
         </Container>
@@ -46,12 +54,14 @@ class App extends React.Component {
         <div id="tabs-container">
           <Tabs fill defaultActiveKey="home" transition={false} id="noanim-tab-example">
             <Tab eventKey="home" title="Ingredients">
-              <div id = 'img-container'>
-              <Images style = {{width:'100px', height:'100px'}} ingredients = {ingredients}/>
+              <div id='img-container'>
+              <ImagesIngredients style = {{width:'100px', height:'100px'}} ingredients = {ingredients}/>
               </div>
             </Tab>
             <Tab eventKey="profile" title="Utensils">
-              utensils
+              <div id='img-container2'>
+              <ImagesUtensils style = {{width:'100px', height:'100px'}} utensils = {utensils}/>
+              </div>
             </Tab>
             <Tab eventKey="contact" title="Recipe">
               recipe
