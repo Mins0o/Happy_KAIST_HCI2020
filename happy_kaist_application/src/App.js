df93c6915b6e2e5c7bd6333e2ee97ccafb8de9a5
Hyunseung-Lim <chrlie9807@kaist.ac.kr>
2020-06-08 02:00:54 +0900

    change app.js

--- happy_kaist_application/src/App.js
+++ happy_kaist_application/src/App.js
@@ -1,21 +1,17 @@
-import React from 'react';
-import './App.css';
+import React, { Component } from 'react';
+import { Route } from 'react-router-dom';
+import { Mainpage, Recipe } from 'pages';
 
 
-import MainPage from "./components/main_page.js";
-
-
-class App extends React.Component {
-	
-	render() {
-	  return (
-
-		<div className="App">
-		<br/><br/><br/>
-			<MainPage/>
-		</div>
-
-	  );
-	}
+class App extends Component {
+    render() {
+        return (
+            <div>
+                <Route exact path="/" component={Mainpage}/>
+                <Route path="/about" component={Recipe}/>
+            </div>
+        );
+    }
 }
+
 export default App;
