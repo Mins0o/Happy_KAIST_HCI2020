23355403ba3ffb6177659f205ab66508e34a9f07
Hyunseung-Lim <chrlie9807@kaist.ac.kr>
2020-06-09 07:39:26 +0900

    checkout!

--- happy_kaist_application/src/pages/Checkoutpage.js
+++ happy_kaist_application/src/pages/Checkoutpage.js
@@ -2,43 +2,60 @@ import React from 'react';
 import './Checkoutpage.css';
 import HeaderBar from '../components/minsoo/HeaderBar.js';
 import Table from 'react-bootstrap/Table';
+import Tablebar from '../components/checkout/tablebar';
+import Button from 'react-bootstrap/Button';
+import Popup from '../components/checkout/checkoutbox';
 
 class Checkout extends React.Component {
+  constructor() {
+    super();
+    this.state = {
+      showPopup: false
+    };
+  }
+
+  togglePopup() {
+    this.setState({
+      showPopup: !this.state.showPopup
+    });
+  }
 	
 	render() {
 	  return (
 		<div className = "checkout">
             <HeaderBar/>
-            <br/><br/><br/><br/>
             <div className = "tableForm">
                 <Table striped bordered hover>
                     <thead>
                         <tr>
                         <th>#</th>
                         <th>Name</th>
-                        <th>Buy or Rent</th>
+                        <th>Type</th>
                         <th>Count</th>
                         <th>Price</th>
                         </tr>
                     </thead>
                     <tbody>
-                        <tr>
-                        <td>1</td>
-                        <td>Mark</td>
-                        <td>Otto</td>
-                        <td>@mdo</td>
-                        <td>@mdo</td>
-                        </tr>
-                        <tr>
-                        <td>2</td>
-                        <td>Jacob</td>
-                        <td>Thornton</td>
-                        <td>@fat</td>
-                        <td>@mdo</td>
-                        </tr>
+                      <Tablebar tnum = {1} tname = {"tomato"} ttype = {"Buy"} tcount = {1} tprice = {1.3}/>
+                      <Tablebar tnum = {1} tname = {"tomato"} ttype = {"Buy"} tcount = {1} tprice = {1.3}/>
+                      <Tablebar tnum = {1} tname = {"tomato"} ttype = {"Buy"} tcount = {1} tprice = {1.3}/>
+                      <tr>
+                      <td colSpan="4">total</td>
+                      <td>4.22</td>
+                      </tr>
                     </tbody>
                 </Table>
-            </div>            
+                <div className = "paymentButton">
+                  <Button onClick = {this.togglePopup.bind(this)}>pay a charge</Button>
+                </div>
+            </div>      
+        {this.state.showPopup ? 
+          <Popup
+            text='Close Me'
+            closePopup={this.togglePopup.bind(this)}
+          />
+          : null
+        }      
 		</div>
 	  );
 	}
